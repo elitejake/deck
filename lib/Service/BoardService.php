9a19a45dd0fd8fee9c7245337585068bb70ed68f
Sergey Shliakhov <husband.sergey@gmail.com>
2020-06-04 15:41:32 +0200

    Move code to BoardService
    Signed-off-by: Sergey Shliakhov <husband.sergey@gmail.com>

--- lib/Service/BoardService.php
+++ lib/Service/BoardService.php
@@ -30,6 +30,7 @@ use OCA\Deck\AppInfo\Application;
 use OCA\Deck\Db\Acl;
 use OCA\Deck\Db\AclMapper;
 use OCA\Deck\Db\AssignmentMapper;
+use OCA\Deck\Db\CardMapper;
 use OCA\Deck\Db\ChangeHelper;
 use OCA\Deck\Db\IPermissionMapper;
 use OCA\Deck\Db\Label;
@@ -68,6 +69,7 @@ class BoardService {
 	private $activityManager;
 	private $eventDispatcher;
 	private $changeHelper;
+	private $cardMapper;
 
 	private $boardsCache = null;
 
@@ -82,6 +84,7 @@ class BoardService {
 		PermissionService $permissionService,
 		NotificationHelper $notificationHelper,
 		AssignmentMapper $assignedUsersMapper,
+		CardMapper $cardMapper,
 		IUserManager $userManager,
 		IGroupManager $groupManager,
 		ActivityManager $activityManager,
@@ -104,6 +107,7 @@ class BoardService {
 		$this->eventDispatcher = $eventDispatcher;
 		$this->changeHelper = $changeHelper;
 		$this->userId = $userId;
+		$this->cardMapper = $cardMapper;
 	}
 
 	/**
@@ -668,6 +672,19 @@ class BoardService {
 		}
 
 		return $newBoard;
+	}
+
+	/**
+	 * @param $ownerId
+	 * @param $newOwnerId
+	 * @return void
+	 */
+	public function transferOwnership($owner, $newOwner)
+	{
+		$this->boardMapper->transferOwnership($owner, $newOwner);
+		$this->assignedUsersMapper->transferOwnership($owner, $newOwner);
+		$this->aclMapper->transferOwnership($owner, $newOwner);
+		$this->cardMapper->transferOwnership($owner, $newOwner);
 	}
 
 	private function enrichWithStacks($board, $since = -1) {
