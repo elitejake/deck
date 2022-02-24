b40060f54e664049057e1bea1cf5c70ff46ebbd0
Julius Härtl <jus@bitgrid.net>
2022-02-24 16:43:37 +0100

    Add typing to jsonSerialize
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- lib/Db/RelationalObject.php
+++ lib/Db/RelationalObject.php
@@ -23,7 +23,9 @@
 
 namespace OCA\Deck\Db;
 
-class RelationalObject implements \JsonSerializable {
+use JsonSerializable;
+
+class RelationalObject implements JsonSerializable {
 	protected $primaryKey;
 	protected $object;
 
@@ -38,7 +40,7 @@ class RelationalObject implements \JsonSerializable {
 		$this->object = $object;
 	}
 
-	public function jsonSerialize() {
+	public function jsonSerialize(): array {
 		return array_merge(
 			['primaryKey' => $this->primaryKey],
 			$this->getObjectSerialization()
@@ -50,9 +52,9 @@ class RelationalObject implements \JsonSerializable {
 	 *
 	 * @throws \Exception
 	 */
-	public function getObjectSerialization() {
-		if ($this->object instanceof \JsonSerializable) {
-			$this->object->jsonSerialize();
+	public function getObjectSerialization(): array {
+		if ($this->object instanceof JsonSerializable) {
+			return $this->object->jsonSerialize();
 		} else {
 			throw new \Exception('jsonSerialize is not implemented on ' . get_class($this));
 		}
